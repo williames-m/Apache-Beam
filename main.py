376a398150abda4ac832bdb4412e78f454b63f2c
William Machado <96849274+williames-m@users.noreply.github.com>
2022-05-29 17:29:54 -0300

    Add files via upload
    Main contendo todo o código.
    
    Os arquivos são a geração dos dados.

---
+++ main.py
@@ -0,0 +1,161 @@
+import re
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io.textio import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+pipeline_options = PipelineOptions(argv=None)
+pipeline = beam.Pipeline(options=pipeline_options)
+
+
+colunas_dengue = ['id',
+                'data_iniSE',
+                'casos',
+                'ibge_code',
+                'cidade',
+                'uf',
+                'cep',
+                'latitude',
+                'longitude']
+
+def lista_para_dicionario(elemento, colunas):
+    """
+    Recebe 2 listas
+    Retorna 1 dicionário
+    """
+    return dict(zip(colunas, elemento))
+
+def texto_para_lista(elemento, delimitador='|'):
+    """
+    Recebe um texto e um delimitador
+    Retorna uma lista de elementos pelo delimitador
+    """
+    return elemento.split(delimitador)
+
+def trata_data(elemento):
+    """
+    Recebe um dicionário e cria um novo campo com ANO-MES
+    Retorna o mesmo dicionário com o novo campo
+    """
+    elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
+    return elemento
+    
+
+def chave_uf(elemento):
+    """ 
+    Receber um dicionário
+    Retornar uma tupla com o estado(UF) e o elemento (UF, dicionário)
+    """
+    chave = elemento['uf']
+    return(chave, elemento)
+
+def casos_dengue(elemento):
+    """[
+    Recebe uma tupla ('RS', [{},{}])
+    Retorna uma tupla ('RS-2014-12', 8.0)
+    """
+    uf, registros = elemento
+    for registro in registros:
+        if bool(re.search(r'\d', registro['casos'])):
+            yield (f"{uf}-{registro['ano_mes']}", float(registro['casos']))
+        else:
+            yield (f"{uf}-{registro['ano_mes']}", 0.0)
+    
+def chave_uf_ano_mes_de_lista(elemento):
+    """ 
+    Receber uma lista de elementos
+    Retornar uma tupla contendo uma chave e o valor de chuva em mm
+    ('UF-ANO-MES', 1.3)
+    ['2015-06-27', '0.4', 'ES']
+    """
+    data, mm, uf = elemento
+    ano_mes = '-'.join(data.split('-')[:2])
+    chave = f'{uf}-{ano_mes}'
+    if float(mm) < 0:
+        mm = 0.0
+    else:
+        mm = float(mm)
+    return chave, mm
+
+def arredonda(elemento):
+    """ 
+    Recebe uma tupla ('RO-2019-02', 1470.4000000000026)
+    Retorna uma tupla com o valor arredondado ('RO-2019-02', 1470.4)
+    """
+    chave, mm = elemento
+    return (chave, round(mm, 1))
+
+def filtra_campos_vazios(elemento):
+    """ 
+    Remove elementos que tenham chaves vazias
+    Recebe uma tupla ('CE-2015-01', {'chuvas': [85.8], 'dengue': [175.0]})
+    Retorna uma tupla ('CE-2015-01', {'chuvas': [85.8], 'dengue': [175.0]})
+    """
+    chave, dados = elemento
+    if all([
+        dados['chuvas'], 
+        dados['dengue']
+        ]):
+        return True
+    return False
+
+def descompactar_elementos(elemento):
+    """ 
+    Receber uma tupla ('CE-2015-11', {'chuvas': [0.4], 'dengue': [21.0]})
+    Retornar uma tupla ('CE','2015','11','0.4','21.0')
+    """
+    chave, dados = elemento
+    chuva = dados['chuvas'][0]
+    dengue = dados['dengue'][0]
+    uf, ano, mes = chave.split('-')
+    return uf, ano, mes, str(chuva), str(dengue)
+
+def preparar_csv(elemento, delimitador=';'):
+    """ 
+    Receber uma tupla ('CE', 2015, 11, 0.4, 21.0)
+    Retornar uma string delimitada "CE; 2015; 11; 0.4; 21.0"
+    """
+    return f"{delimitador}".join(elemento)
+
+# print('Casos de dengue')
+dengue = (
+    pipeline
+    | "Leitura do dataset de dengue" >> ReadFromText('casos_dengue.txt', skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
+    | "Criar campo ano_mes" >> beam.Map(trata_data)
+    | "Criar chave pelo estado" >> beam.Map(chave_uf)
+    | "Agrupar pelo estado" >> beam.GroupByKey()
+    | "Descompactar casos de dengue" >> beam.FlatMap(casos_dengue)
+    | "Soma dos casos pela chave" >> beam.CombinePerKey(sum)
+    # | "Mostrar resultados" >> beam.Map(print)
+)
+
+# print('Quantidade de chuva')
+chuvas = (
+    pipeline
+    | "Leitura do dataset de chuvas" >> ReadFromText('chuvas.csv', skip_header_lines=1)
+    | "De csv para lista (chuvas)" >> beam.Map(texto_para_lista, delimitador=',')
+    | "Criando a chave UF-ANO-MES" >> beam.Map(chave_uf_ano_mes_de_lista)
+    | "Soma total das chuvas pela chave" >> beam.CombinePerKey(sum)
+    | "Arredondar resultados de chuvas" >> beam.Map(arredonda)
+    # | "Mostrar resultados de chuvas" >> beam.Map(print)
+)
+
+resultado = (
+    # (chuvas, dengue)
+    # | "Empilha as pcollections" >> beam.Flatten()
+    # | "Agrupa as pcollections" >> beam.GroupByKey()
+    ({'chuvas': chuvas, 'dengue': dengue})
+    | "Mesclar pcols" >> beam.CoGroupByKey()
+    | "Filtrar dados vazios" >> beam.Filter(filtra_campos_vazios)
+    | "Descompactar elementos" >> beam.Map(descompactar_elementos)
+    | "Preparar csv" >> beam.Map(preparar_csv)
+    # | "Mostrar resultados da união" >> beam.Map(print)
+)
+
+header = "UF;ANO;MES;CHUVA;DENGUE"
+
+resultado | "Criar arquivo CSV" >> WriteToText('resultado', file_name_suffix='.csv', header=header)
+
+pipeline.run()
